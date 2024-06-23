3a0c0dd8910d79d537ae1a1cd0f949d16b9af0b1
Anirudh Yadav <anirudhyadavallen@gmail.com>
2024-06-23 15:20:58 +0530

    Here's the Code for Bellman Ford Algorithm For negative edge weight Cycles

---
+++ BellManFordAlgorithm.cpp
@@ -0,0 +1,49 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+                        /*
+                                    Here's the Code for Bellman Ford
+                                                    Dijkstra's Algorithm                                    BellmanFord Algorithm
+                                                        O(E+E*log(V))                   |                       O(V*E)
+                                                For positive weight cycles              |               For negative weight cycles 
+                        */
+
+void bellman_ford(map<int,vector<pair<int,int>>> &graph, int src, int V){
+    vector<int> distance(V,INT_MAX);
+    distance[src] = 0;
+
+    for(int k=0; k<V-1; k++){           // O(V)
+        // O(E)
+        for(int i=0; i<V; i++){
+            for(auto it : graph[i]){                       // NEIGHBORS OF i'th ELEMENT
+                int u = i;
+                int v = it.first;
+                int wt= it.second;
+                if(distance[i] != INT_MAX && distance[i] + wt < distance[v]){
+                    distance[v] = distance[i] + wt;
+                }
+            }
+        }
+    }
+
+    for(int i=0; i<distance.size(); i++){   
+        cout << distance[i] << " ";
+    }
+}
+
+void make_edge(map<int,vector<pair<int,int>>> &graph, int u, int v, int wt){
+    graph[u].push_back(make_pair(v,wt));
+}
+
+int main(){
+    map<int,vector<pair<int,int>>> graph;
+    make_edge(graph, 0, 1, 2);
+    make_edge(graph, 1, 2, -4);
+    make_edge(graph, 0, 2, 4);
+    make_edge(graph, 2, 3, 2);
+    make_edge(graph, 3, 4, 4);
+    make_edge(graph, 4, 1, -1);
+    int V = graph.size();
+    bellman_ford(graph, 0, V);
+    return 0;
+}
